d8a44b07b5e71d4045d78411fc3f17f961ae8126
Ying Huang <ying.huang@cohesity.com>
2023-02-08 15:11:12 -0800

    add description in shell.

--- Solutions/CohesitySecurity/Data Connectors/Helios2Sentinel/zip.test.sh
+++ Solutions/CohesitySecurity/Data Connectors/Helios2Sentinel/zip.test.sh
@@ -1,4 +1,8 @@
 #!/bin/zsh
+# Use IDE to publish to folder: bin/Release/net6.0/publish.bak
+# then unzip the generated zip file to a temporary folder, and use kdiff3 to compare it with publish.bak.
+# So as to test whether the zip file generated from CLI is identical to the one by IDE.
+#
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
