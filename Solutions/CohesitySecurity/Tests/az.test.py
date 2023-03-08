f79c23c252da279ed01d8ffe14c0586a63efdc1c
Ying Huang <ying.huang@cohesity.com>
2023-03-07 16:22:49 -0800

    rename files. add comments, small changes according to review feedback.

--- Solutions/CohesitySecurity/Tests/az.test.py
+++ Solutions/CohesitySecurity/Tests/az.test.py
@@ -54,11 +54,11 @@ class TestCohesity(unittest.TestCase):
         # self.skipTest("Skipping test_cohesity_close_helios_incident")
         playbook_name = "Cohesity_Close_Helios_Incident"
         subscription_id = get_subscription_id()
-        vid, alert_id = get_one_incident_id(self.resource_group, self.workspace_name)
+        incident_id, alert_id = get_one_incident_id(self.resource_group, self.workspace_name)
 
         alert_details = get_alert_details(alert_id, self.api_key)
         self.assertEqual(alert_details['alertState'], "kOpen")  # maybe we need to close incident after close helios alert, otherwise, this assert might fail.
-        returncode = run_playbook(subscription_id, vid, self.resource_group, self.workspace_name, playbook_name)
+        returncode = run_playbook(subscription_id, incident_id, self.resource_group, self.workspace_name, playbook_name)
         self.assertEqual(returncode, 0)
 
         time.sleep(30)  # Sleep for 30 seconds
@@ -74,7 +74,7 @@ class TestCohesity(unittest.TestCase):
         that the number of alerts in Helios matches the number of incidents in the Sentinel workspace.
         """
         ids = get_incident_ids(self.resource_group, self.workspace_name)
-        alert_ids = [alert_id for (vid, alert_id) in ids]
+        alert_ids = [alert_id for (incident_id, alert_id) in ids]
         for alert_id in alert_ids:
             self.assertIsNotNone(get_alert_details(alert_id, self.api_key), f"alert_id --> {alert_id} doesn't exist in helios.")
         alerts_details = get_alerts_details(alert_ids, self.api_key)
@@ -85,7 +85,7 @@ class TestCohesity(unittest.TestCase):
         This test case verifies that there are no duplicate incidents in the Sentinel workspace.
         """
         ids = get_incident_ids(self.resource_group, self.workspace_name)
-        alert_ids = [alert_id for (vid, alert_id) in ids]
+        alert_ids = [alert_id for (incident_id, alert_id) in ids]
         return len(alert_ids) != len(np.unique(np.array(alert_ids)))
 
     def test_alerts_in_sentinel(self):
